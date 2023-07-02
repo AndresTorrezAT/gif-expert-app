4dc58dde3c8b25377b52f8cec2af1a20116521d3
[Andres] <[andres.torrez.work@gmail.com]>
2023-07-01 23:53:26 -0400

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,17 @@
+
+export const GifExpertApp = () => {
+  return (
+    <>
+        {/* Titulo */}
+        <h1>GifExpertApp</h1>
+
+        {/* Input */}
+
+        {/* Listado de Gif */}
+
+            {/* Gif Item */}
+    </>
+  )
+}
+
+// video 6
