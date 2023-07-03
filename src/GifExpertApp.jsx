eb3721c028313a0a53fc682790bbb894fa5e0da4
[Andres] <[andres.torrez.work@gmail.com]>
2023-07-02 20:15:35 -0400

    Se agrego un onSubmit y onNewCategory

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,13 +1,37 @@
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState([ 'One Puch', 'Dragon Ball']);
+
+  const onAddCategory = ( newCategory ) => {
+    // console.log(newCategory);
+    setCategories([ newCategory, ...categories ]);
+    // setCategories( cat => [ ...cat, 'Valorant' ]);
+  }
+
   return (
     <>
         {/* Titulo */}
         <h1>GifExpertApp</h1>
 
         {/* Input */}
+        <AddCategory 
+          // setCategories={ setCategories } 
+          onNewCategory={ (value) => onAddCategory(value) }
+        />
 
         {/* Listado de Gif */}
+        <ol>
+          {
+            categories.map( category => {
+              return <li key={ category }>{ category }</li>
+            })
+          }
+
+          {/* <li>ABC</li> */}
+        </ol>
 
             {/* Gif Item */}
     </>
@@ -15,3 +39,5 @@ export const GifExpertApp = () => {
 }
 
 // video 6
+
+// AyOAbVAdFoCFnl4PTCgHaZ9MGtVnxSru
