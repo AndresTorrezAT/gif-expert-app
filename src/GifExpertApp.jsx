9103a3cd4f45dc8d337359409e9203a4217c9825
[Andres] <[andres.torrez.work@gmail.com]>
2023-07-03 18:28:52 -0400

    Se añadio el grid que genera datos

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,43 +1,38 @@
 import { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
 
-  const [categories, setCategories] = useState([ 'One Puch', 'Dragon Ball']);
+  const [categories, setCategories] = useState([ 'One Puch' ]);
 
   const onAddCategory = ( newCategory ) => {
-    // console.log(newCategory);
+
+    if( categories.includes(newCategory) ) return;
+
     setCategories([ newCategory, ...categories ]);
-    // setCategories( cat => [ ...cat, 'Valorant' ]);
+    // setCategories( cat => [ ...cat, 'Valorant' ]); // Otra forma
   }
 
   return (
     <>
-        {/* Titulo */}
         <h1>GifExpertApp</h1>
 
-        {/* Input */}
         <AddCategory 
-          // setCategories={ setCategories } 
-          onNewCategory={ (value) => onAddCategory(value) }
+          // setCategories={ setCategories } // mala practica
+          onNewCategory = { (value) => onAddCategory(value) }
         />
 
-        {/* Listado de Gif */}
-        <ol>
-          {
-            categories.map( category => {
-              return <li key={ category }>{ category }</li>
-            })
-          }
-
-          {/* <li>ABC</li> */}
-        </ol>
-
-            {/* Gif Item */}
+        {
+          categories.map( ( category ) => (
+              <GifGrid 
+                key={ category } 
+                category={ category }
+              />
+          ))
+        }
     </>
   )
 }
-
-// video 6
 
 // AyOAbVAdFoCFnl4PTCgHaZ9MGtVnxSru
